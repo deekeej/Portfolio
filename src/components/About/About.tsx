7e72e6b7c860fbb1d15b2c78ba9507f70bc43895
deekeej <96180674+deekeej@users.noreply.github.com>
2023-05-16 19:57:46 +0200

    About component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"></img>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worlwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              ut, nihil sed alias qui assumenda rem! Nam sint ut unde eos
+              ratione temporibus veritatis ipsum animi modi, sit quidem illo?
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Let`s Talk
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
