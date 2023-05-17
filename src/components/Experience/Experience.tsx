642cf32dbc2d49d5d811cbc0f02497f3dcdd432d
deekeej <96180674+deekeej@users.noreply.github.com>
2023-05-17 23:05:33 +0200

    Experience component in pogress

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,8 +1,47 @@
-import React from "react";
 import "./Experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience_backend"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
